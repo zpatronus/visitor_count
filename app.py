f2f287f32b341c149e798ec6083f748d50db21f3
Zijun Yang <zijunhz@126.com>
2024-11-26 11:24:26 -0500

    add cors support

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, abort
+from flask_cors import CORS
 import sqlite3
 
 app = Flask(__name__)
+CORS(app)
 
 def init_db():
     """Initialize the database if it doesn't exist."""
